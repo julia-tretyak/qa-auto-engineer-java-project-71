6fe494cc718c3055212a80d4b777e006aa3c2b9a
Юлия Третьяк <julia@tretyak.com>
2026-06-24 14:34:02 +0300

    Add README.md

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -4,16 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Differ {
     public static String generate(String filePath1, String filePath2) throws Exception {
-        String content1 = readFile(filePath1);
-        String content2 = readFile(filePath2);
+        Map<String, Object> data1 = parseJson(readFile(filePath1));
+        Map<String, Object> data2 = parseJson(readFile(filePath2));
 
-        Map<String, Object> data1 = parseJson(content1);
-        Map<String, Object> data2 = parseJson(content2);
-
-        return "Data1: " + data1 + "\nData2: " + data2;
+        return buildDiff(data1, data2);
     }
 
     private static String readFile(String filePath) throws Exception {
@@ -23,5 +25,54 @@ public class Differ {
     private static Map<String, Object> parseJson(String content) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(content, Map.class);
+    }
+
+    private static String buildDiff(Map<String, Object> data1, Map<String, Object> data2) {
+        Set<String> allKeys = new HashSet<>(data1.keySet());
+        allKeys.addAll(data2.keySet());
+
+        Map<String, Object> sortedKeys = new TreeMap<>();
+        for (String key : allKeys) {
+            sortedKeys.put(key, null);
+        }
+
+        List<String> lines = new ArrayList<>();
+        lines.add("{");
+
+        for (String key : sortedKeys.keySet()) {
+            boolean inFirst = data1.containsKey(key);
+            boolean inSecond = data2.containsKey(key);
+
+            if (inFirst && inSecond) {
+                Object value1 = data1.get(key);
+                Object value2 = data2.get(key);
+
+                if (value1.equals(value2)) {
+                    lines.add("    " + key + ": " + formatValue(value1));
+                } else {
+                    lines.add("  - " + key + ": " + formatValue(value1));
+                    lines.add("  + " + key + ": " + formatValue(value2));
+                }
+            } else if (inFirst) {
+                lines.add("  - " + key + ": " + formatValue(data1.get(key)));
+            } else {
+                lines.add("  + " + key + ": " + formatValue(data2.get(key)));
+            }
+        }
+
+        lines.add("}");
+        return String.join("\n", lines);
+    }
+
+    private static String formatValue(Object value) {
+        if (value instanceof String) {
+            return (String) value;
+        } else if (value instanceof Boolean) {
+            return value.toString();
+        } else if (value instanceof Number) {
+            return value.toString();
+        } else {
+            return String.valueOf(value);
+        }
     }
 }
